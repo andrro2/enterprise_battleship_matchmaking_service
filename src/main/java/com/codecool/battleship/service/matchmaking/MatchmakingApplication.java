93e18c8d3f9fbce1e431aa2fa111008482851eea
Rozner András <roznernek@gmail.com>
2020-08-05 13:56:48 +0200

    initial commit

---
+++ src/main/java/com/codecool/battleship/service/matchmaking/MatchmakingApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.battleship.service.matchmaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchmakingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchmakingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/battleship/service/matchmaking/MatchmakingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.battleship.service.matchmaking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchmakingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
