44799474ab7f1d46e962653b11a6c7890609b250
markligeti <ligetimark.e@gmail.com>
2020-08-06 13:17:15 +0200

    application name set, route added for testing of zuul

--- src/main/java/com/codecool/battleship/service/matchmaking/MatchmakingApplication.java
+++ src/main/java/com/codecool/battleship/service/matchmaking/MatchmakingApplication.java
@@ -2,9 +2,17 @@ package com.codecool.battleship.service.matchmaking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class MatchmakingApplication {
+
+    @RequestMapping(value = "/")
+    public String home() {
+        return "Hello! I make matches...";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MatchmakingApplication.class, args);
